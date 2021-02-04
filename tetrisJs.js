c99a62712dc8d5b0678b6e7dd3f997581bda5550
oktoberdead <78501382+oktoberdead@users.noreply.github.com>
2021-02-04 18:45:56 +0300

    Update tetrisJs.js

--- tetrisJs.js
+++ tetrisJs.js
@@ -1,51 +1,184 @@
+const fieldHeight = 20;
+const fieldWidth = 11;
+const cellSize = 25;
+
+
 let mainFr = document.getElementById("mainFrame");
 let button = document.getElementById("startButton");
 let isStarted = 0;
 
-		mainFr.style.left = window.innerWidth / 2 - 205 + "px"; 
-		button.style.left = window.innerWidth / 2 - 107 + "px";
-
-
-function rand(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-
-
-window.onresize = function( event ) {
-	if(window.innerWidth > 420) {
-		mainFr.style.left = window.innerWidth / 2 - 205 + "px";
-		button.style.left = window.innerWidth / 2 - 107 + "px";
-
-}
-};
-
+let type;
+let figure = [0];
+let figurePos = [0];
 
 let isGameOver = 0;
-let fastSpeed = 30;
+let fastSpeed = 35;
 let slowSpeed = 130;
 
-
 let figRed, figGreen, figBlue, figAlpha;
-
 
 let cells = [];
 let isOccupied = [0]
 
-for(let i = 0; i < 128; i++){
+let I = 0;
+
+let fallSpeed = slowSpeed;
+let N = Math.floor(fieldWidth / 2);
+let isSmthPressed = 0;
+let numberOfFigures = 0;
+
+
+mainFr.style.gridTemplateColumns = "repeat(" + fieldWidth + ", " + cellSize + "px)";
+mainFr.style.gridTemplateRows = "repeat(" + fieldHeight + ", " + cellSize + "px)";
+
+mainFr.style.height = fieldHeight * cellSize + "px";
+mainFr.style.width = fieldWidth * cellSize + "px";
+
+function rand(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function Rcolor() {
+	figRed = rand(50, 200);
+	figBlue = rand(50, 200);
+	figGreen = rand(50, 200);
+	figAlpha = rand(50, 100);
+	
+}
+
+
+
+
+let currentFigure = document.createElement("div");
+	currentFigure.id = "curFig";
+	Rcolor();
+	currentFigure.style.background = "rgba("+figRed+","+figGreen+","+figBlue+","+figAlpha/100+")"; 
+	currentFigure.style.height = cellSize - 2 + "px";
+	currentFigure.style.width = cellSize - 2 + "px";
+
+
+
+
+
+
+
+
+
+	window.onresize = function sizeControl(event){
+		
+			mainFr.style.left = window.innerWidth / 2 - (cellSize+2)*Math.floor(fieldWidth / 2); + "px"; 
+			button.style.left = window.innerWidth / 2 - 107 + "px";
+		
+}
+if(window.innerWidth > fieldWidth * (cellSize+2)) {
+			mainFr.style.left = window.innerWidth / 2 - (cellSize+2)*Math.floor(fieldWidth / 2) + "px"; 
+			button.style.left = window.innerWidth / 2 - 107 + "px";
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function T_shape() {
+	
+	for(let i = 0; i < 3; i++){
+		figure[i] = document.createElement("div");
+		figure[i].id = "figBlock" + i;
+		Rcolor();
+		figure[i].style.background = "rgba("+figRed+","+figGreen+","+figBlue+","+figAlpha/100+")"; 
+
+	}
+
+}
+
+
+function Z_shape(type) {
+	
+}
+
+function L_shape() {
+	
+}
+
+function I_shape() {
+	
+}
+
+function Square_shape() {
+	
+}
+
+
+
+
+function spawnFigure(){
+	switch(rand(1, 6)){
+
+case 1:
+T_shape();
+break;
+
+
+
+case 2:
+Square_shape();
+break;
+
+
+
+case 3:
+L_shape();
+break;
+
+
+
+case 4:
+I_shape();
+break;
+
+
+
+case 5:
+Z_shape(1);
+break;
+
+
+
+case 6:
+Z_shape(2);
+break;
+
+	}
+}
+
+
+
+
+
+
+
+
+for(let i = 0; i < fieldWidth*fieldHeight; i++){
 	cells[i] = document.createElement("div");
 	cells[i].id = "c" + i;
 	cells[i].style.border = "solid 1px black";
 	mainFr.append(cells[i]);
 }
 
-let fallSpeed = 120;
-let N = 4;
-let isSmthPressed = 0;
-let numberOfFigures = 0;
 
 document.addEventListener('keydown', function(event) {
 
@@ -57,12 +190,12 @@ case "KeyS": fallSpeed = fastSpeed;
 break;
 case "KeyA": 
 
-if(N % 8 > 0 && !isSmthPressed) N--;
+if(N % fieldWidth > 0 && !isSmthPressed && !isOccupied[N-1]) N--;
 isSmthPressed = 1;
 break;
 case "KeyD": 
-;
-if(N % 8 < 7 && !isSmthPressed) N++;
+
+if(N % fieldWidth < fieldWidth-1 && !isSmthPressed && !isOccupied[N+1]) N++;
 isSmthPressed = 1
 break;
 case "KeyW":
@@ -81,24 +214,15 @@ if(event.code == "KeyS") fallSpeed = slowSpeed;
 }
 )
 
-let currentFigure = document.createElement("div");
-currentFigure.id = "curFig";
-	figRed = rand(0, 256);
-	figBlue = rand(0, 256);
-	figGreen = rand(0, 256);
-	figAlpha = rand(0, 100);
-	currentFigure.style.background = "rgba("+figRed+","+figGreen+","+figBlue+","+figAlpha/100+")"; 
-
-let I = 0;
 
 function gameTick(){
 
 
 
 cells[N].append(currentFigure);
-if(N < 120 && I % 3 == 0){
-	N += 8;
-} else if(N >= 120 || isOccupied[N+8] == 1) {
+if(N < fieldWidth*fieldHeight - fieldWidth && I % 3 == 0){
+	N += fieldWidth;
+} else if(N >= fieldWidth*fieldHeight - fieldWidth || isOccupied[N+fieldWidth] == 1) {
 	freezeInPos();
 }
 
@@ -119,20 +243,19 @@ setTimeout(gameTick, 1300);
 
 function freezeInPos(){
 
-	if(N > 7){
+	if(N > fieldWidth - 1){
 	currentFigure.id = "block" + numberOfFigures;
 	currentFigure.style.background = "rgba("+figRed+","+figGreen+","+figBlue+","+figAlpha/100+")"; 
-	currentFigure.style.height = "48px"; 
-	currentFigure.style.width = "48px";
+	currentFigure.style.height = cellSize - 2 + "px"; 
+	currentFigure.style.width = cellSize - 2 + "px";
 	currentFigure = document.createElement("div");
 	currentFigure.id = "curFig";
 	isOccupied[N] = 1;
-	figRed = rand(50, 200);
-	figBlue = rand(50, 200);
-	figGreen = rand(50, 200);
-	figAlpha = rand(50, 100);
+	Rcolor();
 	currentFigure.style.background = "rgba("+figRed+","+figGreen+","+figBlue+","+figAlpha/100+")"; 
-	N = 4;
+	currentFigure.style.height = cellSize - 2 + "px"; 
+	currentFigure.style.width = cellSize - 2 + "px";
+	N = Math.floor(fieldWidth / 2);
 	numberOfFigures++;
 }
 else {
